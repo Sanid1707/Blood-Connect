0051bb0b9a1deeed05c631e613aace7bad03b794
Sanidhya Pandey <pandeysanid@gmail.com>
2025-03-14 11:45:29 +0000

    Initial Commit

---
+++ BloodConnect/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BloodConnect
+//
+//  Created by Sanidhya Pandey on 14/03/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ BloodConnectTests/BloodConnectTests.swift
@@ -0,0 +1,17 @@
+//
+//  BloodConnectTests.swift
+//  BloodConnectTests
+//
+//  Created by Sanidhya Pandey on 14/03/2025.
+//
+
+import Testing
+@testable import BloodConnect
+
+struct BloodConnectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
